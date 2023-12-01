167e7383910ebf877973ee8c086e03ad86824e23
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 <temesgenfeni@gmail.com>
2023-12-01 22:58:40 +0300

    Done

--- Week_2/bmi.js
+++ Week_2/bmi.js
@@ -1,4 +1,4 @@
-Simple javaScript program to calculate bmi.
+//Simple javaScript program to calculate bmi.
 const mass = parseFloat(prompt("Enter you mass(in kilograms)"));
 const height = parseFloat(prompt("Enter your height (in meters)"));
 const bmi = mass / (height * height);
